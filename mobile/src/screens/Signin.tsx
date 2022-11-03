55a94ae47f2737c550692a6468b2c87dc3aec962
Otácio Barbosa <otacio.barbosa@outlook.com>
2022-11-02 22:46:54 -0300

    Started the Mobile Project

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { Text, Center } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bg="gray.900" alignItems={"center"}>
+            <Text color="white" fontSize={24} fontFamily="heading">
+            SignIn
+            </Text>
+        </Center>
+    )
+}
