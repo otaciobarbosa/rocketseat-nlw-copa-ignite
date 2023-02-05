9817c9185c9ea0c129ac185e6485f6b88f09f56f
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-02-05 18:35:33 -0300

    Update Loading.tsx

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,9 +1,9 @@
 import { Center, Spinner } from "native-base";
 
-export function Loading(){
-    return(
+export function Loading() {
+  return (
     <Center flex={1} bg="gray.900">
-        <Spinner color="yellow.500"/>
-    </Center>
-    )
+      <Spinner color="yellow.500" />
+    </Center >
+  );
 }
